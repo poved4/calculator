aa0859d5af009a718bed23acb9a72318a6a8d407
poved4 <poved4@gmail.com>
2021-10-14 13:01:32 -0500

    working calculator class

--- js/script.js
+++ js/script.js
@@ -15,25 +15,37 @@ class Calculator {
     }
 
     ac() {
-        this.x = undefined; 
-        this.y = undefined;
+        //Display
         this.result = '';
-        this.currentOperator = '';
-        this.operation = undefined;
+        this.operation = '';
+
+        //Operadores logicos
         this.prevOperator = undefined;
+        this.currentOperator = undefined;
         
         this.updateDisplay();
     }
 
-    del() { this.operation = this.operation.toString().slice(0, -1); }
+    del() { 
+        this.operation = this.operation.toString().slice(0, -1); 
+        this.updateDisplay();
+    }
 
-    equals() {
-        this.calculate();
+    calculate(isTrue = true) { 
+        const symbol = isTrue ? this.currentOperator : this.prevOperator;
+        const compute = this.operation.split(symbol);
+        const x = parseFloat(compute[0]);
+        const y = parseFloat(compute[1]);
+        this.result = this.#operations[symbol](x, y); 
+        
+        if (!isTrue) { 
+            this.prevOperator = undefined;
+            this.operation = this.result.toString(); 
+        }
+        
         this.updateDisplay();
     }
     
-    calculate (operator) { this.#operations[operator](this.x, this.y); }
-
     addOperator(operator) {
         if(this.currentOperator === operator) return
 
@@ -41,20 +53,30 @@ class Calculator {
         else { 
             this.prevOperator = this.currentOperator;
             this.currentOperator = operator;
-            this.calculate(this.prevOperator);
+            this.calculate(false);
         }
 
+        this.operation += this.currentOperator;
         this.updateDisplay();
     }
 
     addNumber(number) {
 
+        if (this.currentOperator === undefined) {
+            if (number === '.' && this.operation.includes('.')) return;
+        } else {
+            const symbol = this.prevOperator === undefined ? this.currentOperator : this.prevOperator;
+            const compute = this.operation.split(symbol);
+            if (number === '.' && compute[1].includes('.')) return;
+        }
+
+        this.operation += number.toString();
         this.updateDisplay();
     }
 
     updateDisplay() {
         this.resultOperation.innerHTML = this.result;
-        this.currentOperation.innerHTML = this.currentOperator;
+        this.currentOperation.innerHTML = this.operation;
     }
 }
 
@@ -63,7 +85,7 @@ const calculator = new Calculator('currentOperation', 'resultOperation')
 const methods = {
     "allClear":   ()  => { calculator.ac(); },
     "delete":     ()  => { calculator.del(); },
-    "equals":     ()  => { calculator.equals(); },
+    "equals":     ()  => { calculator.calculate(); },
     "number":     (x) => { calculator.addNumber(x); },
     "operation":  (x) => { calculator.addOperator(x); }
 }
