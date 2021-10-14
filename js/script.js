45cbc0541f4e789ba4365631d497a931c318812a
poved4 <poved4@gmail.com>
2021-10-14 10:53:54 -0500

    work in the calculator class

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
 "use strict";
 
 class Calculator {
-    constructor () {
-        this.operations = {
-            "+": (x, y) => x + y,
-            "-": (x, y) => x - y,
-            "*": (x, y) => x * y,
-            "/": (x, y) => y > 0 ? x / y : 'Math Error'
-        }
+    #operations = {
+        "+": (x, y) => x + y,
+        "-": (x, y) => x - y,
+        "*": (x, y) => x * y,
+        "/": (x, y) => y > 0 ? x / y : 'Math Error'
+    }
 
+    constructor(currentOperation, resultOperation) {
+        this.currentOperation = document.getElementById(currentOperation);
+        this.resultOperation = document.getElementById(resultOperation);
         this.ac();
     }
 
-    ac () {
+    ac() {
         this.x = undefined; 
         this.y = undefined;
+        this.result = '';
+        this.currentOperator = '';
         this.operation = undefined;
         this.prevOperator = undefined;
-        this.currentOperator = undefined;
+        
+        this.updateDisplay();
     }
 
-    del = () => this.operation = this.operation.toString().slice(0, -1);
+    del() { this.operation = this.operation.toString().slice(0, -1); }
 
-    addOperator (operator) {
+    equals() {
+        this.calculate();
+        this.updateDisplay();
+    }
+    
+    calculate (operator) { this.#operations[operator](this.x, this.y); }
+
+    addOperator(operator) {
         if(this.currentOperator === operator) return
 
         if(this.currentOperator === undefined) this.currentOperator = operator;
@@ -31,9 +43,34 @@ class Calculator {
             this.currentOperator = operator;
             this.calculate(this.prevOperator);
         }
+
+        this.updateDisplay();
     }
 
-    calculate = (operator) => this.operations[operator](this.x, this.y);
+    addNumber(number) {
+
+        this.updateDisplay();
+    }
+
+    updateDisplay() {
+        this.resultOperation.innerHTML = this.result;
+        this.currentOperation.innerHTML = this.currentOperator;
+    }
 }
 
-const calculator = new Calculator();
+const calculator = new Calculator('currentOperation', 'resultOperation')
+
+const methods = {
+    "allClear":   ()  => { calculator.ac(); },
+    "delete":     ()  => { calculator.del(); },
+    "equals":     ()  => { calculator.equals(); },
+    "number":     (x) => { calculator.addNumber(x); },
+    "operation":  (x) => { calculator.addOperator(x); }
+}
+
+document.querySelectorAll('.controls button')
+.forEach(button => {
+    button.addEventListener('click', () => {
+        methods[button.className || button.id](button.innerHTML);
+    });
+}); 
