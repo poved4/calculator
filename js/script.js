588a3b4313f17e5bd87f7b94e6c8436ffafcd74c
poved4 <poved4@gmail.com>
2021-10-14 09:14:10 -0500

    doing calculator methods

--- js/script.js
+++ js/script.js
@@ -1,104 +1,39 @@
-let result = '';
-let operator = undefined;
-let currentOperation = '';
-let prevOperator = undefined;
+"use strict";
 
-
-const del = document.getElementById('delete');
-const equals = document.getElementById('equals');
-const allClear = document.getElementById('allClear');
-
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operation');
-
-const operation = document.getElementById('currentOperation');
-const operationResult = document.getElementById('operationResult');
-
-
-function updateDisplay() {
-    operation.innerHTML = currentOperation;
-    operationResult.innerHTML = result;
-}
-
-function compute() {
-    let computation;
-    const z = prevOperator === undefined ? operator : prevOperator;
-    let numbers = currentOperation.split(z);
-    
-    const x = parseFloat(numbers[0]);
-    const y = parseFloat(numbers[1]);
-
-    switch (z) {
-        case '+': computation = x + y; break;
-        case '-': computation = x - y; break;
-        case '*': computation = x * y; break;
-        case '/': if(y !== 0) computation = x / y; else computation = 'Math Error'; break;
-        default: computation = 'Math Error'; break;
-    }
-
-    if (prevOperator !== undefined) { 
-        prevOperator = undefined;
-        currentOperation = `${computation}`; 
-    }
-    result = computation;
-}
-
-
-/* --Get Numbers Buttons List-- */
-numbers.forEach(button => {
-    /* --Add Listener to Buttons-- */
-    button.addEventListener('click', () => { 
-        
-        /* Append Numbers to Current Operation */
-        const number = button.innerText;
-        if(number === '.' && currentOperation.includes('.')) return
-        currentOperation += number.toString();
-        updateDisplay();
-    });
-}); 
-
-/* --Get Operator Buttons List-- */
-operators.forEach(button => {
-    /* --Add Listener to Buttons-- */
-    button.addEventListener('click', () => {
-
-        const btnValue = button.innerText;
-
-        /* Is the current operator the same as the new operator */
-        if(operator === btnValue) return
-        
-        /* the current operator is equal to undefined, it does assign new value */
-        if (operator === undefined) { operator = btnValue; }
-        else { 
-            /* 
-                the current operator is not equal to undefined. 
-                assign the value of the current operator to the previous one.
-                call the function compute.
-            */
-            prevOperator = operator; 
-            operator = btnValue; 
-            compute();
+class Calculator {
+    constructor () {
+        this.operations = {
+            "+": (x, y) => x + y,
+            "-": (x, y) => x - y,
+            "*": (x, y) => x * y,
+            "/": (x, y) => y > 0 ? x / y : 'Math Error'
         }
-        
-        currentOperation += operator.toString();
-        updateDisplay();
-    });
-});
 
-equals.addEventListener('click', () => {
-    compute();
-    updateDisplay();
-});
+        this.ac();
+    }
 
-allClear.addEventListener('click', () => {
-    result = '';
-    operator = undefined;
-    currentOperation = '';
-    prevOperator = undefined;
-    updateDisplay();
-});
+    ac () {
+        this.x = undefined; 
+        this.y = undefined;
+        this.operation = undefined;
+        this.prevOperator = undefined;
+        this.currentOperator = undefined;
+    }
 
-del.addEventListener('click', () => {
-    currentOperation = currentOperation.toString().slice(0, -1);
-    updateDisplay();
-});
+    del = () => this.operation = this.operation.toString().slice(0, -1);
+
+    addOperator (operator) {
+        if(this.currentOperator === operator) return
+
+        if(this.currentOperator === undefined) this.currentOperator = operator;
+        else { 
+            this.prevOperator = this.currentOperator;
+            this.currentOperator = operator;
+            this.calculate(this.prevOperator);
+        }
+    }
+
+    calculate = (operator) => this.operations[operator](this.x, this.y);
+}
+
+const calculator = new Calculator();
